faa2acb24ffbe9a64217b25ce166e420296b14ec
marcoscaldas <donarumo@hotmail.com>
2020-09-21 19:08:47 -0300

    Obtendo um post por id

---
+++ src/main/java/com/marcoscaldas/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.marcoscaldas.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcoscaldas.workshopmongo.domain.Post;
+import com.marcoscaldas.workshopmongo.domain.User;
+import com.marcoscaldas.workshopmongo.dto.UserDTO;
+import com.marcoscaldas.workshopmongo.repository.PostRepository;
+import com.marcoscaldas.workshopmongo.repository.UserRepository;
+import com.marcoscaldas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired   // para instanciar o repository
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+
+}
